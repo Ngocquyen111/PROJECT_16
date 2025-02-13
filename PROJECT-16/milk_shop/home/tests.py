8aba7a30a545e6dcb6983117f42006468fe1f5b5
trieuduong09 <duongnt6865@ut.edu.vn>
2025-02-13 17:18:24 +0700

    Update tests.py

--- PROJECT-16/milk_shop/home/tests.py
+++ PROJECT-16/milk_shop/home/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.urls import path
+from . import views
 
-# Create your tests here.
+urlpatterns = [
+    path('', views.milkshop, name='home'),
+    path('members/', views.milkshop, name='members'),
+]
