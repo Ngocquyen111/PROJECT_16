4dbf0b64242f95c6764be4fe08095590877dd694
trieuduong09 <duongnt6865@ut.edu.vn>
2025-02-13 20:14:04 +0700

    Update views.py

--- PROJECT-16/milk_shop/home/views.py
+++ PROJECT-16/milk_shop/home/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def milkshop(request):
-    return HttpResponse("<h1>Welcome to My Milk Shop!</h1>")
+# Create your views here.
