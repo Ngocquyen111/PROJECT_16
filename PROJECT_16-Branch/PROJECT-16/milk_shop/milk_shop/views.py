43f06d2f8ad2e5e308372f4e5e7b7a212edf5839
trieuduong09 <duongnt6865@ut.edu.vn>
2025-02-18 19:41:32 +0700

    Cap nhat code

---
+++ PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def checkout(request):
+    return render(request, 'milk_shop/checkout.html')
+
+def process_checkout(request):
+    if request.method == "POST":
+        full_name = request.POST['full_name']
+        phone = request.POST['phone']
+        address = request.POST['address']
+        email = request.POST.get('email', '')
+        payment_method = request.POST['payment_method']
+
+        if payment_method == "bank_transfer":
+            return redirect('payment_qr')  # Chuyển hướng đến trang QR Code
+        
+        return HttpResponse("Đặt hàng thành công! Nhân viên sẽ liên hệ bạn để xác nhận.")
+
+def payment_qr(request):
+    return render(request, 'milk_shop/payment_qr.html')
