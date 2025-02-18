373ca5e78ca516a17032483d4ec9ffc12707380c
trieuduong09 <duongnt6865@ut.edu.vn>
2025-02-18 20:13:53 +0700

    them ung dung

--- PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/urls.py
+++ PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.milk_shop, name='home'),
-    path('members/', views.milk_shop, name='members'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('process_checkout/', views.process_checkout, name='process_checkout'),
+    path('payment_qr/', views.payment_qr, name='payment_qr'),
+    path('vouchers/', views.voucher_list, name='voucher_list'),
+    path('voucher/<int:voucher_id>/', views.voucher_detail, name='voucher_detail'),
 ]
