dcffd2bdba19d7e644cc7172cddeac09e4646810
Nynn1507 <ynnn8596@ut.edu.vn>
2025-02-19 13:07:21 +0700

    cap nhat code

--- PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/urls.py
+++ PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from . import views
+from .views import article_list, article_detail
 
 urlpatterns = [
-<<<<<<< HEAD
+
+    path("articles/", article_list, name="article_list"),
+    path("articles/<int:article_id>/", article_detail, name="article_detail"),
     path('checkout/', views.checkout, name='checkout'),
     path('process_checkout/', views.process_checkout, name='process_checkout'),
     path('payment_qr/', views.payment_qr, name='payment_qr'),
     path('vouchers/', views.voucher_list, name='voucher_list'),
     path('voucher/<int:voucher_id>/', views.voucher_detail, name='voucher_detail'),
-=======
     path('', views.milk_shop, name='home'),
     path('members/', views.milk_shop, name='members'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
->>>>>>> d5399da (Them code)
+
 ]
--- PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/views.py
+++ PROJECT_16-Branch/PROJECT-16/milk_shop/milk_shop/views.py
@@ -1,6 +1,16 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, "app_home/article_list.html", {"articles": articles})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, "app_home/article_detail.html", {"article": article})
 
 def checkout(request):
     return render(request, 'milk_shop/checkout.html')
