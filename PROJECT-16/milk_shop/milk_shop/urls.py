216454f47c229d2f0c062e21f043e877a1269cbb
PhamTanThong03 <ptthong.www@gmail.com>
2025-02-22 17:13:07 +0700

    Them sp

--- PROJECT-16/milk_shop/milk_shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.milk_shop, name='home'),
-    path('members/', views.milk_shop, name='members'),
-]
--- PROJECT-16/milk_shop/milk_shop/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def milkshop(request):
-    return HttpResponse("<h1>Welcome to My Milk Shop!</h1>")
